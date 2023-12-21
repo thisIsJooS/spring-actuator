0c522b3a3073225fb3255881a43c6f69c6de633f
Seungwoo, Joo <thisis.joos@gmail.com>
2023-12-21 20:17:06 +0900

    feat: actuator test

---
+++ src/main/java/hello/order/v4/OrderServiceV4.java
@@ -0,0 +1,41 @@
+package hello.order.v4;
+
+import hello.order.OrderService;
+import io.micrometer.core.annotation.Timed;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Timed("my.order")
+@Slf4j
+public class OrderServiceV4 implements OrderService {
+    private AtomicInteger stock = new AtomicInteger(100);
+
+    @Override
+    public void order(){
+        log.info("주문");
+        stock.decrementAndGet();
+        sleep(500);
+    }
+
+    @Override
+    public void cancel() {
+        log.info("취소");
+        stock.incrementAndGet();
+        sleep(200);
+    }
+
+    @Override
+    public AtomicInteger getStock() {
+        return stock;
+    }
+
+    private static void sleep(int t){
+        try{
+            Thread.sleep(t + new Random().nextInt(200));
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
